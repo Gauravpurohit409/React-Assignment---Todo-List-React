a804d6257c9202f581b8f0f9c9dff1f0b4edc40f
Gauravpurohit409 <gauravpurohit6666@gmail.com>
2021-02-13 00:39:50 +0530

    completed

---
+++ src/components/Add/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const Add = ({task,setTask}) => {
+
+const  handleClick = (event) =>{
+    console.log(event.target.value);
+   setTask(event.target.value);
+}
+
+     return (
+        <>
+            <textarea className="ml-3 mr-2" id = "task" placeholder="Enter the task todo" value = {task} onChange = {handleClick}></textarea>
+         </>
+    );
+};
+
+export default Add;
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,96 @@
+
 import React from "react";
+import { Button } from "react-bootstrap";
 import "./../styles/App.css";
+import Add from "./Add";
 
 function App() 
 {
-	return (
+	let srno = 0;
+	let [task,setTask] = React.useState();
+
+	const deleteTask = (event) => {
+		const div = event.target.parentElement;
+		const divRoot = div.parentElement;
+		console.log(div,divRoot);
+		divRoot.removeChild(div);
+	}
+	const changeValue = (input,btn) =>{
+		console.log(input,btn);
+		if (input.value.length === 0){
+			btn.disabled = true;
+
+		}
+		else{
+			btn.disabled = false;
+			btn.addEventListener("click",edit);
+			btn.innerHTML = "Edit";
+			input.disabled = true;
+			setTask("");
+		}
+	} 
+	
+
+	const edit = (event) => {
+
+		console.log(event.target.parentElement);
+
+	//	console.log(event.target.parentElement.childNodes[0]);
+		const input = event.target.parentElement.childNodes[0];
+		input.addEventListener("change",(inputObj) =>{
+			event.target.disabled = false;
+			setTask(inputObj.target.value);
+		});
+		input.disabled = false;
+		
+		event.target.innerHTML = "Save";
+
+		event.target.removeEventListener("click",edit);	
+		event.target.addEventListener("click",(btn) =>{
+			// console.log("btn is ",btn);
+			changeValue(input,btn.target);
+		});
+	}
+	const addInRoot = () =>{
+	
+		const divRoot = document.getElementById("root");
+		const div = document.createElement("div");
+		div.setAttribute("classname","list");
+	
+		const input  = document.createElement("input");
+		input.setAttribute("id","taskArea");
+		input.value = task;
+		input.disabled = true;
+		input.style.marginRight = "30px";
+		input.style.marginLeft ="20px";
+		div.appendChild(input);
+
+		const editBtn = document.createElement("button");
+		editBtn.setAttribute("classname","edit");
+		editBtn.innerHTML = "Edit";
+		editBtn.style.marginLeft = "20px";
+		editBtn.addEventListener("click",edit);
+		div.appendChild(editBtn);
+
+		const delBtn = document.createElement("button");
+		delBtn.setAttribute("classname","delete");
+		delBtn.innerHTML = "Delete";
+		delBtn.style.marginLeft = "20px";
+		delBtn.addEventListener("click",deleteTask);
+		div.appendChild(delBtn);
+
+		divRoot.appendChild(div);
+
+		setTask("");
+	}
+   return (
 	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
+		<>
+		<h1> Todo List </h1>
+		<Add task = {task} setTask = {setTask} />
+		<Button id= "btn" variant="secondary" onClick = {addInRoot}>Add</Button>
+       
+		</>
 	</div>
 	);
 }
